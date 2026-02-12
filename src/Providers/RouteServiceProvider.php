b20faebbc9062b026ec71021b451dc7111fad9e6
Ernesto Alvarez López <alvarez93lopez@gmail.com>
2026-02-12 15:06:37 -0500

    v1.4.2. Usar InitializeTenancyByRequestData

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace Ingenius\Products\Providers;
 
-use Ingenius\Core\Http\Middleware\InitializeTenancyByDomain;
-use Ingenius\Core\Http\Middleware\PreventAccessFromCentralDomains;
+use Ingenius\Core\Http\Middleware\InitializeTenancyByRequestData;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
@@ -81,8 +80,7 @@ class RouteServiceProvider extends ServiceProvider
 
         if (file_exists($routeFile)) {
             Route::middleware([
-                InitializeTenancyByDomain::class,
-                PreventAccessFromCentralDomains::class,
+                InitializeTenancyByRequestData::class
             ])->group(function () use ($routeFile) {
                 require $routeFile;
             });
